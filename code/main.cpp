5bea53cfd886d88c9597b8d3d7a465245bc65219
Hanafi1824 <hax180204@gmail.com>
2023-02-01 22:27:49 +0800

    editing the output

--- code/main.cpp
+++ code/main.cpp
@@ -17,6 +17,7 @@ int main() {
 
     cout << "Enter your name: ";
     getline(cin, username);
+    system("cls");
 
     // capitalize each first letter of the name
     for (int i = 0; i < username.length(); i++) {
@@ -32,6 +33,7 @@ int main() {
     cout << "Are you ready to start the program? (Y/N): ";
     cin >> ready;
     ready = toupper(ready);
+    system("cls");
 
     while (ready == 'Y') {
         cout << " 1. January    2. February     3. March " << endl;
@@ -40,18 +42,22 @@ int main() {
         cout << " 10. October   11. November    12. December " << endl;
         cout << "Enter the month: ";
         cin >> months;
+        system("cls");
 
         cout << "Enter your income: RM ";
         cin >> income;
+        system("cls");
 
         cout << " 1. Food       2. Housing          3. Utilities        4. Transportation " << endl;
         cout << " 5. Clothing   6. Health Care      7. Entertainment    8. Personal Care " << endl;
         cout << " 9. Education  10. Miscellaneous " << endl;
         cout << " Choose category for your spending: ";
         cin >> categorySpend;
+        system("cls");
 
         cout << "Enter your spending: RM ";
         cin >> spending;
+        system("cls");
 
         flowOfCash = cashFlow(income, spending);
         cout << "Your cash flow for this month is: RM " << flowOfCash << endl;
@@ -59,10 +65,10 @@ int main() {
         cout << "Do you want to continue? (Y/N): ";
         cin >> ready;
         ready = toupper(ready);
+        system("cls");
     }
 
     cout << "\nThank you for using our program." << endl;
-
     system("pause");
 
     return 0;
