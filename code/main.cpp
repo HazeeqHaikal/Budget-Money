8422ccb6f5721a495f5de9975e2a8f8f27fad39a
HazeeqHaikal <haikalroslan740@gmail.com>
2023-02-02 15:19:11 +0800

    find most spending from each category and display at the very last

--- code/main.cpp
+++ code/main.cpp
@@ -12,8 +12,8 @@ int main() {
     int months, categorySpend;
     float income, spending, flowOfCash;
     char ready;
-    string end;
     string username;
+    string categorySpendData[10][2] = {{"Food", "0"}, {"Housing", "0"}, {"Utilities", "0"}, {"Transportation", "0"}, {"Clothing", "0"}, {"Health Care", "0"}, {"Entertainment", "0"}, {"Personal Care", "0"}, {"Education", "0"}, {"Miscellaneous", "0"}};
 
     cout << "Enter your name: ";
     getline(cin, username);
@@ -53,6 +53,42 @@ int main() {
         cout << "Enter your spending: RM ";
         cin >> spending;
 
+        switch (categorySpend) {
+            case 1:
+                categorySpendData[0][1] = to_string(stof(categorySpendData[0][1]) + spending);
+                break;
+            case 2:
+                categorySpendData[1][1] = to_string(stof(categorySpendData[1][1]) + spending);
+                break;
+            case 3:
+                categorySpendData[2][1] = to_string(stof(categorySpendData[2][1]) + spending);
+                break;
+            case 4:
+                categorySpendData[3][1] = to_string(stof(categorySpendData[3][1]) + spending);
+                break;
+            case 5:
+                categorySpendData[4][1] = to_string(stof(categorySpendData[4][1]) + spending);
+                break;
+            case 6:
+                categorySpendData[5][1] = to_string(stof(categorySpendData[5][1]) + spending);
+                break;
+            case 7:
+                categorySpendData[6][1] = to_string(stof(categorySpendData[6][1]) + spending);
+                break;
+            case 8:
+                categorySpendData[7][1] = to_string(stof(categorySpendData[7][1]) + spending);
+                break;
+            case 9:
+                categorySpendData[8][1] = to_string(stof(categorySpendData[8][1]) + spending);
+                break;
+            case 10:
+                categorySpendData[9][1] = to_string(stof(categorySpendData[9][1]) + spending);
+                break;
+            default:
+                cout << "Invalid input." << endl;
+                break;
+        }
+
         flowOfCash = cashFlow(income, spending);
         cout << "Your cash flow for this month is: RM " << flowOfCash << endl;
 
@@ -61,6 +97,18 @@ int main() {
         ready = toupper(ready);
         system("cls");
     }
+
+    // cout << "Your spending for each category: " << endl;
+    string spendMost;
+    float spendMostValue = 0;
+    for (int i = 0; i < 10; i++) {
+        if (stof(categorySpendData[i][1]) > spendMostValue) {
+            spendMostValue = stof(categorySpendData[i][1]);
+            spendMost = categorySpendData[i][0];
+        }
+    }
+
+    cout << "You spend the most on " << spendMost << " with RM " << spendMostValue << endl;
 
     cout << "\nThank you for using our program." << endl;
     system("pause");
