0f7a0886fcb32b4ecd6525a3f4736a749d4dff1c
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2023-02-02 13:36:36 +0800

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -17,7 +17,6 @@ int main() {
 
     cout << "Enter your name: ";
     getline(cin, username);
-    system("cls");
 
     // capitalize each first letter of the name
     for (int i = 0; i < username.length(); i++) {
@@ -33,7 +32,6 @@ int main() {
     cout << "Are you ready to start the program? (Y/N): ";
     cin >> ready;
     ready = toupper(ready);
-    system("cls");
 
     while (ready == 'Y') {
         cout << " 1. January    2. February     3. March " << endl;
@@ -42,22 +40,18 @@ int main() {
         cout << " 10. October   11. November    12. December " << endl;
         cout << "Enter the month: ";
         cin >> months;
-        system("cls");
 
         cout << "Enter your income: RM ";
         cin >> income;
-        system("cls");
 
         cout << " 1. Food       2. Housing          3. Utilities        4. Transportation " << endl;
         cout << " 5. Clothing   6. Health Care      7. Entertainment    8. Personal Care " << endl;
         cout << " 9. Education  10. Miscellaneous " << endl;
         cout << " Choose category for your spending: ";
         cin >> categorySpend;
-        system("cls");
 
         cout << "Enter your spending: RM ";
         cin >> spending;
-        system("cls");
 
         flowOfCash = cashFlow(income, spending);
         cout << "Your cash flow for this month is: RM " << flowOfCash << endl;
